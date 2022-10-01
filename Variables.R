b3a2e0509f995c1c9a5daccd338632b58aa313d8
adejumoridwan <you@example.com>
2022-10-01 17:05:39 +0100

    update

---
+++ Variables.R
@@ -0,0 +1,33 @@
+name <- "Joseph"
+age <- "32"
+
+#name_2 <- "Mary"
+
+
+# calling by name
+name
+age
+
+# calling using the print function
+print(name)
+print(age)
+
+
+
+
+
+#valid variable names
+myvar <- "Apple"
+my_var <- "Apple"
+myVar <- "Apple"
+MYVAR <- "Apple"
+myvar2 <- "Apple"
+.myvar <- "Apple"
+
+#invalid variable names
+2myvar <- "Apple"
+my-var <- "Apple"
+my var <- "Apple"
+_my_var <- "Apple"
+my_v@ar <- "Apple"
+ <- "Apple"
